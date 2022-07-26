900639b3589da4ceb5032215ff990cf9c2ea51a2
xiongqiang <1379716219@qq.com>
2022-07-26 16:14:02 +0800

    初始化项目结构

---
+++ gam-service/src/main/java/org/xq/GamApplication.java
@@ -0,0 +1,14 @@
+package org.xq;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author xiongqiang
+ * @version 1.0
+ */
+
+public class GamApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
